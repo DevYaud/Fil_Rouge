333c04c423d0e4a97738538412714825299e6d61
Mael M <mallet.mael@protonmail.com>
2025-03-21 16:11:50 +0100

    a

--- www/conn_db.php
+++ www/conn_db.php
@@ -11,6 +11,7 @@ $DATABASE_NAME = 'sc1zuna1689_fil_rouge';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
     // If there is an error with the connection, stop the script and display the error.
+    echo "Non connecté";
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 ?>
