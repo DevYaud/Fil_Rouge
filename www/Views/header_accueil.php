7761dd900dcb9223ca46eca5a854eb277bc125d5
Lionel <lionel.songoro@protonmail.com>
2025-03-23 16:40:56 +0100

    Set files "View" and "Controller"

---
+++ www/Views/header_accueil.php
@@ -0,0 +1,17 @@
+
+<?php
+echo'<ul class="navbar">
+        <li class="dropdown nav_element">
+            <a class="drop">Menu</a>
+            <div class="dropdown-content">
+                <a href="/public">Notre mission</a>
+                <a href="/about.php">Pourquoi nous ?</a>
+                <a href="/login.php">Connexion</a>
+                <a href="/inscription.php">Formulaire / Inscription</a>
+                <a href="/mentions.php">Mentions légales</a>
+                <a href="/cgv.php">CGV</a>
+            </div>
+        </li>
+        <li class="nav_element">Les bambins</li>
+     </ul>'
+?>
