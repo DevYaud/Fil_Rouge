111c6c6509c495642c6175f8bcb0c5369f8b5ff1
Lionel <lionel.songoro@protonmail.com>
2025-03-25 19:56:39 +0100

    Update "Activité" class

--- www/Models/Activite.php
+++ www/Models/Activite.php
@@ -16,4 +16,17 @@ class Activite
         $this->groupe = $_groupe;
         $this->nb_max = $_nb_max;
     }
+    public function getId_activite() { return $this->Id_activite;}
+    public function getNom(): string {return $this->nom;}
+    public function getGroupe() { return $this->groupe;}
+    public function getNb_max() { return $this->nb_max;}
+    public function getDescription() { return $this->description;}
+    public function getId_specialite() { return $this->Id_Specialite;}
+    public function getId_thematique() { return $this->Id_thematique;}
+    //---
+    public function setNom(string $nom) { $this->nom = $nom;}
+    public function setDescription($description) { $this->description = $description;}
+    public function setGroupe($groupe) { $this->groupe = $groupe;}
+    public function setNb_max($nb_max) { $this->nb_max = $nb_max;}
+
 }
