82c56c7127a2ba57faf09883786dd4ad03efdf56
Lionel <lionel.songoro@protonmail.com>
2025-03-26 19:51:15 +0100

    added CSS parents (WIP)

--- www/app/repas/desinscription_repas.php
+++ www/app/repas/desinscription_repas.php
@@ -8,3 +8,20 @@ $con = getDatabase();
 
 include '../navigation_parents.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire de Contact</title>
+    <link rel="stylesheet" href="../../styles/main_parent.css">
+    <link rel="stylesheet" href="../../styles/form.css">
+</head>
+
+<body>
+    <main class="content">
+
+
+    </main>
+</body>
+</html>
