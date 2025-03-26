ccd63c2ced9fb42f33a74f82f0414da937f8aa21
Mael M <mallet.mael@protonmail.com>
2025-03-26 17:33:50 +0100

    Ajouts travail restant

---
+++ www/app/dashboard.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+require_once '../functions/session.php';
+verifierSessionParents();
+
+require_once '../functions/db.php';
+$con = getDatabase();
+
+include 'navigation_parents.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Personnel</title>
+    <link rel="stylesheet" href="../styles/main.css">
+</head>
+
+<body>
+<main class="content">
+    <div class="container">
+        <h1 class="text-center">Tableau de bord - Personnel</h1>
+
+        <div class="grid">
+
+            <div class="box">
+                <h2>Emploi du temps</h2>
+
+            </div>
+
+            <div class="box">
+                <h2>Informations de connexion :</h2>
+                <p class="btn"><strong>Email :</strong> <?php echo htmlspecialchars($_SESSION['mail']); ?></p>
+                <p class="btn"><strong>ID Connexion :</strong> <?php echo htmlspecialchars($_SESSION['Id_connexion']); ?></p>
+            </div>
+
+            <div class="box">
+                <h2>Gestion des activités</h2>
+                <a href=".//creation_activite.php" class="btn">Inscription</a>
+                <a href="./activite/selection_activite.php" class="btn">Désinscription</a>
+            </div>
+
+            <div class="box">
+                <h2>Gestion des Rapports</h2>
+                <a href="./rapport/creation_rapport.php" class="btn">Créer un rapport</a>
+                <a href="./rapport/selection_rapport.php" class="btn">Modifier un rapport existant</a>
+                <a href="rapport/suppression_rapport.php" class="btn">Supprimer un rapport existant</a>
+            </div>
+
+            <div class="box">
+                <h2>Gestion des comptes</h2>
+                <a href="./compte/creation_compte.php" class="btn">Créer un compte</a>
+                <a href="./menu/selection_menu.php" class="btn">Modifier un compte existant</a>
+                <a href="./menu/suppression_menu.php" class="btn">Supprimer un compte existant</a>
+            </div>
+
+            <div class="box">
+                <h2>Gestion des menus</h2>
+                <a href="./menu/creation_menu.php" class="btn">Créer un menu</a>
+                <a href="./menu/selection_menu.php" class="btn">Modifier un menu existant</a>
+                <a href="./menu/suppression_menu.php" class="btn">Supprimer un menu existant</a>
+            </div>
+        </div>
+    </div>
+</main>
+</body>
+</html>
