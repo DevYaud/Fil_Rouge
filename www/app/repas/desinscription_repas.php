3d9363a5f8b8bad77c9a68fc4e57553996922a31
Lionel <lionel.songoro@protonmail.com>
2025-03-26 19:02:57 +0100

    sidebar added for parents

--- www/app/repas/desinscription_repas.php
+++ www/app/repas/desinscription_repas.php
@@ -1,1 +1,10 @@
 <?php
+session_start();
+require_once '../functions/session.php';
+verifierSessionParents();
+
+require_once '../functions/db.php';
+$con = getDatabase();
+
+include '../navigation_parents.php';
+?>
