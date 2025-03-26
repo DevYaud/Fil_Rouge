5d2054d90ca9ff0aa1c4a0e4546681454df9a1fe
Lionel <lionel.songoro@protonmail.com>
2025-03-26 11:45:01 +0100

    test form with table

--- www/admin/rapport/selection_rapport.php
+++ www/admin/rapport/selection_rapport.php
@@ -25,7 +25,29 @@ $stmt->close();
     </head>
     <body>
     <h1>Sélectionner un Rapport à Modifier</h1>
-    <form action="modification_rapport.php" method="post">
+
+    <table>
+        <thead>
+        <tr>
+            <th>Date du rapport</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($rapports as $rapport): ?>
+            <tr style="cursor: pointer;" onclick="document.getElementById('form_<?php echo $rapport['Id_rapport']; ?>').submit();">
+                <td>
+                    <form id="form_<?php echo $rapport['Id_rapport']; ?>" action="modification_rapport.php" method="post">
+                        <input type="hidden" name="rapport" value="<?php echo $rapport['Id_rapport']; ?>">
+                        <?php echo htmlspecialchars("Rapport #{$rapport['Id_rapport']}: {$rapport['Commentaire']}"); ?>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    <!-- CONSERVER la version précédente par précaution.
+        <form action="modification_rapport.php" method="post">
         <select id="rapport" name="rapport" required>
             <?php foreach ($rapports as $rapport): ?>
                 <option value="<?php echo $rapport['Id_rapport']; ?>">
@@ -34,7 +56,7 @@ $stmt->close();
             <?php endforeach; ?>
         </select><br><br>
         <input type="submit" value="Modifier">
-    </form>
+    </form>-->
     </body>
     </html>
 
