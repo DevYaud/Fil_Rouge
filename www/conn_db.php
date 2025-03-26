ffcf94bc33c09233511cdbfbe19aec43a152b0c2
Mael M <mallet.mael@protonmail.com>
2025-03-26 11:39:33 +0100

    Test Fonction getDatabase()

--- www/conn_db.php
+++ www/conn_db.php
@@ -3,22 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-global $con;
-session_start();
+$con = getDatabase();
 
-// Database information
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'sc1zuna1689';
-$DATABASE_PASS = 'fil_rouge_projet';
-$DATABASE_NAME = 'sc1zuna1689_fil_rouge';
-
-// Connection Test
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-    // If there is an error with the connection, stop the script and display the error.
-    echo "Non connecté";
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-} else {
+if ($con) {
     echo "connecté";
+} else {
+    echo "probleme";
 }
 ?>
