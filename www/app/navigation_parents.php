26041f7165613d74bf1228e1450f32043d7d3c7a
Lionel <lionel.songoro@protonmail.com>
2025-03-27 02:00:25 +0100

    Rectification Sidebar (done)

--- www/app/navigation_parents.php
+++ www/app/navigation_parents.php
@@ -13,27 +13,27 @@ session_start();
 <body>
     <nav class="sidebar">
         <div style="padding: 15px; font-size: 22px; text-align: center">📦</div>
-        <a href="./dashboard.php" class="nav-item">
+        <a href="http://creche.zaud.org/app/dashboard.php" class="nav-item">
             <span class="nav-icon">🏠</span>
             <span class="nav-text">Accueil</span>
         </a>
-        <a href="../app/activite/inscription_activite.php" class="nav-item">
+        <a href="http://creche.zaud.org/app/activite/inscription_activite.php" class="nav-item">
             <span class="nav-icon">🎯</span>
             <span class="nav-text">Activités</span>
         </a>
-        <a href="./repas/inscription_repas.php" class="nav-item">
+        <a href="http://creche.zaud.org/app/repas/inscription_repas.php" class="nav-item">
             <span class="nav-icon">🍔</span>
             <span class="nav-text">Repas</span>
         </a>
-        <a href="./autre/facture.php" class="nav-item">
+        <a href="http://creche.zaud.org/app/autre/facture.php" class="nav-item">
             <span class="nav-icon">📜</span>
             <span class="nav-text">Facture</span>
         </a>
-        <a href="./autre/notation.php" class="nav-item">
+        <a href="http://creche.zaud.org/app//autre/notation.php" class="nav-item">
             <span class="nav-icon">⚙️</span>
             <span class="nav-text">Satisfaction</span>
         </a>
-        <a href="./logout.php" class="nav-item">
+        <a href="http://creche.zaud.org/app/logout.php" class="nav-item">
             <span class="nav-icon">🚪</span>
             <span class="nav-text">Se déconnecter</span>
         </a>
